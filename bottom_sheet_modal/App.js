802c0e71f70d83f0760cdc6a8d08349362b972bd
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-26 22:23:09 +0000

    Feat: bottom sheet working

--- bottom_sheet_modal/App.js
+++ bottom_sheet_modal/App.js
@@ -1,12 +1,41 @@
-import "react-native-gesture-handler";
-import { View, Text } from 'react-native'
-import React from 'react'
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+import { View, Text, Button, StyleSheet } from 'react-native'
+import React, { useRef } from 'react'
 import { BottomSheetModal, BottomSheetModalProvider } from "@gorhom/bottom-sheet"
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const bottomSheetModalRef = useRef(null);
+
+  const snapPoints = ["48%"];
+
+  function handlePresentModal() {
+      bottomSheetModalRef.current?.present();
+  }
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Text>App</Text>
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheetModalProvider>
+        <Button title="Present Modal" onPress={handlePresentModal} />
+        <StatusBar style="auto" />
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={0}
+          snapPoints={snapPoints}
+        >
+          <View>
+            <Text>Hello</Text>
+          </View>
+        </BottomSheetModal>
+      </BottomSheetModalProvider>
     </GestureHandlerRootView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "gray",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+})
