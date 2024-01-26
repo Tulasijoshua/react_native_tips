87ce7aad7739c1cc8409c771307df5d8c5cd854f
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-26 22:54:29 +0000

    Feat: adding more contents

--- bottom_sheet_modal/App.js
+++ bottom_sheet_modal/App.js
@@ -1,5 +1,5 @@
 import {GestureHandlerRootView} from "react-native-gesture-handler";
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, Switch } from 'react-native'
 import React, { useRef } from 'react'
 import { BottomSheetModal, BottomSheetModalProvider } from "@gorhom/bottom-sheet"
 import { StatusBar } from "expo-status-bar";
@@ -7,25 +7,33 @@ import { StatusBar } from "expo-status-bar";
 export default function App() {
   const bottomSheetModalRef = useRef(null);
 
-  const snapPoints = ["48%"];
+  const snapPoints = ["25%", "48%", "75%"];
 
   function handlePresentModal() {
       bottomSheetModalRef.current?.present();
   }
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <BottomSheetModalProvider>
-        <Button title="Present Modal" onPress={handlePresentModal} />
-        <StatusBar style="auto" />
-        <BottomSheetModal
-          ref={bottomSheetModalRef}
-          index={0}
-          snapPoints={snapPoints}
-        >
-          <View>
-            <Text>Hello</Text>
-          </View>
-        </BottomSheetModal>
+        <View >
+          <Button title="Present Modal" onPress={handlePresentModal} />
+          <StatusBar style="auto" />
+          <BottomSheetModal
+            ref={bottomSheetModalRef}
+            index={0}
+            snapPoints={snapPoints}
+            backgroundStyle={{borderRadius: 30}}
+          >
+            <View style={styles.contentContainer}>
+              <Text style={styles.title}>Dark mode</Text>
+              <View style={styles.row}>
+                <Text>Dark mode</Text>
+                <Switch />
+              </View>
+            </View>
+          </BottomSheetModal>
+        </View>
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
   )
@@ -37,5 +45,18 @@ const styles = StyleSheet.create({
     backgroundColor: "gray",
     alignItems: "center",
     justifyContent: "center",
+  },
+  contentContainer: {
+    flex: 1, 
+    alignItems: "center",
+    paddingHorizontal: 15,
+  },
+  row: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  title: {
+    fontWeight: "900"
   }
 })
