a97638de38c7c825a817b109268383cdb5827274
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-02 20:42:50 +0000

    Feat: startup files

---
+++ calendar_picker/components/CalendarPicker.jsx
@@ -0,0 +1,15 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+
+export default function CalendarPicker() {
+    const [open, setOpen] = useState(false);
+
+  return (
+    <View>
+      <Text>CalendarPicker</Text>
+      <TouchableOpacity>
+        
+      </TouchableOpacity>
+    </View>
+  )
+}
