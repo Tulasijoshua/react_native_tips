c36bfbcc2469002064e5664db8b1dd7854f3ecb6
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-20 00:09:16 +0000

    Feat: bottom sheet

--- scrollable_bottomSheet/App.js
+++ scrollable_bottomSheet/App.js
@@ -1,16 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Button, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import BottomSheet from './components/BottomSheet';
+import { useCallback, useRef } from 'react';
 
 export default function App() {
+  const  bottomSheetRef = useRef(null)
+
+  const expandHandler = useCallback(() => {
+    bottomSheetRef.current?.expand()
+  }, [])
+
+  const closeHandler = useCallback(() => {
+    bottomSheetRef.current?.close()
+  }, [])
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <SafeAreaView style={styles.container}>
           <StatusBar style="auto" />
-          <BottomSheet snapTo={'50%'} />
+          <Button title='expand' onPress={() => expandHandler()} />
+          <Button title='close' onPress={() => closeHandler()} />
+          <BottomSheet ref={bottomSheetRef} snapTo={'50%'} />
         </SafeAreaView>
       </GestureHandlerRootView>
     </SafeAreaProvider>
@@ -22,7 +34,5 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === 'android' ? 50 : 0,
     flex: 1,
     backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- scrollable_bottomSheet/components/BottomSheet.jsx
+++ scrollable_bottomSheet/components/BottomSheet.jsx
@@ -1,33 +1,69 @@
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
-import React from 'react'
+import React, { useCallback, useImperativeHandle, useRef } from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
-export default function BottomSheet({snapTo}) {
-    const {height} = Dimensions.get('screen')
-    const closeHeight = height;
+const BottomSheet = ({ snapTo }, ref) => {
+  const { height } = Dimensions.get('screen');
+  const closeHeight = height;
+  const percentage = parseFloat(snapTo.replace('%', '')) / 100;
+  const openHeight = height - height * percentage;
+  const topAnimation = useSharedValue(closeHeight);
+  
+  const expand = useCallback(() => {
+    'worklet';
+    topAnimation.value = withTiming(openHeight);
+  }, [openHeight, topAnimation]);
+
+  const close = useCallback(() => {
+    'worklet';
+    topAnimation.value = withTiming(closeHeight);
+  }, [closeHeight, topAnimation]);
+
+  // Creating a mutable ref using useRef
+  const bottomSheetRef = useRef(null);
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      expand,
+      close,
+    }),
+    [expand, close]
+  );
+
+  const animationStyle = useAnimatedStyle(() => {
+    const top = topAnimation.value;
+    return {
+      top,
+    };
+  });
+
   return (
-    <View style={[styles.container, {top: 500}]}>
+    <Animated.View style={[styles.container, animationStyle]} ref={bottomSheetRef}>
       <View style={styles.lineContainer}>
         <View style={styles.line} />
       </View>
-    </View>
-  )
-}
+    </Animated.View>
+  );
+};
+
+export default React.forwardRef(BottomSheet);
 
 const styles = StyleSheet.create({
-    container: {
-        ...StyleSheet.absoluteFillObject,
-        backgroundColor: 'white',
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-    },
-    lineContainer: {
-        alignItems: 'center',
-        marginVertical: 10,
-    },
-    line: {
-        width: 50,
-        height: 4,
-        backgroundColor: 'black',
-        borderRadius: 20,
-    }
-})
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  lineContainer: {
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  line: {
+    width: 50,
+    height: 4,
+    backgroundColor: 'black',
+    borderRadius: 20,
+  },
+});
