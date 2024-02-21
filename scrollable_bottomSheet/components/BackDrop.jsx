9a0338ca2fdc97516c52ab9bdbd02152864cdf9a
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-21 23:34:36 +0000

    Feat: backdrop styling

--- scrollable_bottomSheet/components/BackDrop.jsx
+++ scrollable_bottomSheet/components/BackDrop.jsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import Animated, { interpolate, useAnimatedStyle } from 'react-native-reanimated'
 
 export default function BackDrop({ topAnimation, openHeight, closeHeight, close}) {
+    const backDropAnimation = useAnimatedStyle(() => {
+        const opacity = interpolate(
+            topAnimation.value,
+            [closeHeight, openHeight],
+            [0, 0.5]
+        );
+        const display = opacity === 0 ? 'none' : 'flex';
+        return {
+            opacity,
+        }
+    })
   return (
-    <View style={styles.container}>
+    <Animated.View style={[styles.container, backDropAnimation]}>
       <Text>BackDrop</Text>
-    </View>
+    </Animated.View>
   )
 }
 
