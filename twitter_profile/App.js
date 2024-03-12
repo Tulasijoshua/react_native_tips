5146ef0a48df84ef2abb15377e9eaa7089247210
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-03-12 23:03:24 +0000

    Feat: momoheader

--- twitter_profile/App.js
+++ twitter_profile/App.js
@@ -1,40 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-function generateTweets(limit) {
-  return new Array(limit).fill(0).map((_, index) => {
-    const repetitions = Math.floor(Math.random() * 3) + 1;
-
-    return {
-      key: index.toString(),
-      text: 'Lorem ipsum dolor amet '.repeat(repetitions),
-      author: 'Arnaud',
-      tag: 'eveningkid',
-    };
-  });
-}
-
-const TWEETS = generateTweets(30);
-const HEADER_HEIGHT_EXPANDED = 35;
-const HEADER_HEIGHT_NARROWED = 90;
-
-const PROFILE_PICTURE_URI =
-  'https://pbs.twimg.com/profile_images/975388677642715136/7Hw2MgQ2_400x400.jpg';
-
-const PROFILE_BANNER_URI =
-  'https://pbs.twimg.com/profile_banners/3296259169/1438473955/1500x500';
-
-  export default function WrappedApp() {
-    // Keeps notches away
-    return (
-      <SafeAreaProvider>
-        <App />
-      </SafeAreaProvider>
-    );
-  }
-
-function App() {
-  const insets = useSafeAreaInsets();
+export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ twitter_profile/components/MomoHeader.jsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet, StatusBar, ScrollView } from 'react-native'
+import React from 'react'
+import { WINDOW_HEIGHT } from '../constants'
+
+export default function MomoHeader() {
+  return (
+    <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.header}></View>
+        <ScrollView>
+            <View style={styles.scrollViewContent} />
+        </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    header: {
+        position: 'absolute',
+        width: '100%',
+        height: 136,
+        backgroundColor: '#AF0C6E'
+    },
+    scrollViewContent: {
+        height: WINDOW_HEIGHT * 2,
+        backgroundColor: 'white'
+    }
+})
