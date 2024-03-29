e4d1ca6abe2f3b3168673f871544d843c1dfd14a
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-03-29 13:10:28 +0000

    Feat: Share with media

---
+++ media_share/App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import { Alert, Button, Share, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import Fav from './assets/favicon.png';
+
+export default function App() {
+  const onShare = async () => {
+    try {
+      const localUri = FileSystem.documentDirectory + './assets/favicon.png';
+      const result = await Share.share({
+        message: 'React Native | A framework for building native apps using React',
+          url: 'file://' + localUri,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={{fontSize: 40, marginVertical: 40, color: 'black', textAlign: 'center'}}>React Native Share</Text>
+      <TouchableOpacity onPress={onShare} style={{marginTop: 30, alignItems: 'center', backgroundColor: 'cyan', padding: 5}}>
+        <Text style={{fontSize: 15, fontWeight: '500', textTransform: 'capitalize'}}>Share</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+});
